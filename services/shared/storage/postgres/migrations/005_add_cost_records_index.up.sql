7beb33f8f001af68d900c01339987cc1076984b2
Ahmed Soliman <dev.soliman@gmail.com>
2026-04-18 09:48:16 +0200

    refactor: consolidate all schema into 001_initial.up.sql

--- services/shared/storage/postgres/migrations/005_add_cost_records_index.up.sql
+++
@@ -1,7 +0,0 @@
--- 005_add_cost_records_index.up.sql
--- Add index on (tenant_id, period_end) for efficient retention range deletes.
-
-SET search_path TO axiaops;
-
-CREATE INDEX IF NOT EXISTS idx_cost_records_tenant_period_end
-    ON cost_records (tenant_id, period_end);
