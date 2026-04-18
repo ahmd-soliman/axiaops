13593edb0979c6440ec8226e26170597d800e8b3
Ahmed Soliman <dev.soliman@gmail.com>
2026-04-18 09:34:16 +0200

    ci: remove unnecessary down migration

--- services/shared/storage/postgres/migrations/002_add_scan_interval.down.sql
+++
@@ -1,7 +0,0 @@
--- 002_add_scan_interval.down.sql
--- Remove scan_interval_hours column from accounts table.
-
-SET search_path TO axiaops;
-
-ALTER TABLE accounts
-    DROP COLUMN IF EXISTS scan_interval_hours;
